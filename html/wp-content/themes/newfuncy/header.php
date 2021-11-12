c0cc54efb6de61724b58c9cfd31906765985c8fa
David <dmarinoalonso@danielcastelao.org>
2021-11-12 11:31:15 +0100

    commit

---
+++ html/wp-content/themes/newfuncy/header.php
@@ -0,0 +1,3 @@
+<div id="cabecera">
+    <h1>Cabecera Funcy Pro</h1>
+</div>
--- html/wp-content/themes/newfuncy/index.php
+++ html/wp-content/themes/newfuncy/index.php
@@ -1,8 +1,26 @@
 <html>
 <head>
+    <meta charset="utf-8">
     <title>Prueba de PHP</title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css">
 </head>
+
 <body>
-<?php echo '<p>Hola Mundo</p>'; ?>
+<?php get_header()
+?>
+
+
+
+<div id="cuerpo">
+    <?php
+    echo "<h1 class ='morado'> Hola Funcy</h1>";
+    ?>
+
+</div>
+<?php
+
+echo '<p>Hola Mundo</p>'; ?>
+</div>
+<?php get_footer() ?>
 </body>
 </html>
